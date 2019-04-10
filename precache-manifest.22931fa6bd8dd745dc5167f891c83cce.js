721044ff9146323bd98fc93fc999fbba5fca0f66
Chris Weed <chris@workgrid.com>
2019-04-10 14:57:19 -0500

    🔥

---
+++ precache-manifest.22931fa6bd8dd745dc5167f891c83cce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99bcea51ae8e67cab688",
+    "url": "/micro-404-detail/static/css/main.5a54f17e.chunk.css"
+  },
+  {
+    "revision": "99bcea51ae8e67cab688",
+    "url": "/micro-404-detail/static/js/main.178e6af8.chunk.js"
+  },
+  {
+    "revision": "94fbeaf26730a7b09ff2",
+    "url": "/micro-404-detail/static/js/runtime~main.9db69180.js"
+  },
+  {
+    "revision": "791743eaa65e1153a45b",
+    "url": "/micro-404-detail/static/js/2.02074c03.chunk.js"
+  },
+  {
+    "revision": "a3e5ac54a76912783b8d97c282ca22da",
+    "url": "/micro-404-detail/index.html"
+  }
+];
